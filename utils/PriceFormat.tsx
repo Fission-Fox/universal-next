ed1a11c62d53b6af1abb6ccafcde8192770b0910
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2024-06-28 00:01:59 +0500

    fix:"freightchanges"

--- utils/PriceFormat.tsx
+++ utils/PriceFormat.tsx
@@ -6,6 +6,14 @@ const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
+
 export default function PriceFormat({ carPrice }: Props) {
+  if (carPrice === 0) {
+    return (
+      <>
+        <p className="text-sm">Ask for Freight Price</p>
+      </>
+    );
+  }
   return formatter.format(carPrice).replace(/\.?0+$/, "");
 }
